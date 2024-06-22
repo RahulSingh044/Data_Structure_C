4d8a5504b5c2a660ff4a2c82b53d8573320a09f2
Rahul Singh <156307975+RahulSingh044@users.noreply.github.com>
2024-06-22 16:19:40 +0530

    Update GCD.c

--- GCD.c
+++ GCD.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
 
+// Eculiad Distance
+int EUCGCD( int n1, int n2) {
+    while(n1 != n2) {
+     if(n1 > n2) {
+         n1 -= n2;
+     }
+     else
+      n2-=n1;
+    }
+    return n1;
+}
+
 int GCD( int x, int y) {
     if(y == 0) {
         return x;
@@ -12,3 +24,6 @@ int main() {
    int val = GCD(980,9000);
    printf("The GCD is %d",val);
 }
+
+
+
