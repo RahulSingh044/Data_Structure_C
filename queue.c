7fd535a883ca4ea0fdec26f4ef9f298cfe958208
Rahul Singh <156307975+RahulSingh044@users.noreply.github.com>
2024-07-04 13:30:16 +0530

    Create .c file

---
+++ queue.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#define MAX 3
+int queue[MAX];
+int rear = -1;
+int front = -1;
+
+void enqueue(int value) {
+    if(rear >= MAX-1) {
+        printf("Queue is overflow\n");
+        return;
+    }
+    else{
+        if(front == -1) {
+            front = 0;
+        }
+        queue[++rear] = value;
+        printf("The enqueued element in queue is %d\n",value);
+    }
+}
+
+int dequeue() {
+    if(front == -1) {
+        printf("Queue is underflow");
+        return -1;
+    }
+    else {
+        if(front > rear) {
+            front = -1;
+            rear = -1;
+        }
+       printf("The dequeued element is %d ", queue[front++]);
+    }
+}
+
+int main() {
+   enqueue(10);
+   enqueue(20);
+   enqueue(30);
+   dequeue();
+}
