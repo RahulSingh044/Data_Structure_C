be85e6c2c2e2676164c999d49897e040e03a851f
Rahul Singh <156307975+RahulSingh044@users.noreply.github.com>
2024-08-11 23:04:37 +0530

    Update infixToPostfix.c

--- infixToPostfix.c
+++ infixToPostfix.c
@@ -1,117 +1,122 @@
-#include <stdio.h>
-#include <ctype.h>
+#include <stdio.h>   
+#include <ctype.h>   
 
-#define MAX 100
+#define MAX 100    
 
+// Stack and a variable to keep track of the top element
 char stack[MAX];
 int top = -1;
 
+// Function to push an element onto the stack
 void push(char value)
 {
-    if (top < MAX - 1)
+    if (top < MAX - 1)   // Check if there's space in the stack
     {
-        stack[++top] = value;
+        stack[++top] = value;   // Increment the top and add the value to the stack
     }
 }
 
+// Function to pop an element from the stack
 char pop()
 {
-    if (top >= 0)
+    if (top >= 0)   // Check if the stack is not empty
     {
-        return stack[top--];
+        return stack[top--];   // Return the top element and decrement the top
     }
-    return '\0';
+    return '\0';   // Return null character if stack is empty
 }
 
-// for checking the operator in the expression or infix stack
+// Function to check if a character is an operator
 int operator(char value)
 {
     if (value == '+' || value == '-' || value == '/' || value == '*' || value == '^')
     {
-        return 1;
+        return 1;   // Return 1 if the character is an operator
     }
-    return 0;
+    return 0;   // Return 0 if it's not an operator
 }
 
-// checking the precendence of the operator
+// Function to determine the precedence of operators
 int rank(char value)
 {
     if (value == '+' || value == '-')
     {
-        return 1;
+        return 1;   // Lowest precedence for + and -
     }
     else if (value == '/' || value == '*')
     {
-        return 2;
+        return 2;   // Medium precedence for / and *
     }
     else if (value == '^')
     {
-        return 3;
+        return 3;   // Highest precedence for ^
     }
     else
     {
-        return 0;
+        return 0;   // Return 0 for non-operator characters
     }
 }
 
+// Function to convert an infix expression to postfix
 void infixToPostfix(char inf[], char post[])
 {
     int i = 0, j = 0;
 
+    // Loop through the infix expression
     while (inf[i] != '\0')
     {
+        char value = inf[i];   // Get the current character
 
-        char value = inf[i];
-
-        if (isalnum(value))
+        if (isalnum(value))   // If the character is alphanumeric (operand)
         {
-            post[j++] = value;
+            post[j++] = value;   // Add it to the postfix expression
         }
-        else if (value == '(')
+        else if (value == '(')   // If it's an opening parenthesis
         {
-            push(value);
+            push(value);   // Push it onto the stack
         }
-        else if (value == ')')
+        else if (value == ')')   // If it's a closing parenthesis
         {
-            char popele = pop();
+            char popele = pop();   // Pop from the stack until an opening parenthesis is found
             while (popele != '(')
             {
-                post[j++] = popele;
+                post[j++] = popele;   // Add the popped operators to the postfix expression
                 popele = pop();
             }
         }
-        else if (operator(value))
+        else if (operator(value))   // If it's an operator
         {
+            // Pop operators with higher or equal precedence from the stack
             while (top >= 0 && rank(stack[top]) >= rank(value))
             {
                 post[j++] = pop();
             }
-            push(value);
+            push(value);   // Push the current operator onto the stack
         }
         i++;
     }
-     // for checking that all the elements of infix are transferred to postfix
+
+    // Pop the remaining operators from the stack and add them to the postfix expression
     while (top >= 0)
     {
         post[j++] = pop();
     }
-    post[j] = '\0';
+    post[j] = '\0';   // Null-terminate the postfix expression
 }
 
 int main()
 {
-
-    // for storing expression i.e. infix stack
+    // Arrays to store the infix and postfix expressions
     char inf[MAX];
-
-    // for storing expression i.e. postfix stack
     char post[MAX];
 
+    // Prompt the user to enter an infix expression
     printf("Enter the expression :\t");
     scanf("%s", inf);
 
+    // Convert the infix expression to postfix
     infixToPostfix(inf, post);
-    printf("%s", post);
+    printf("%s", post);   // Print the postfix expression
 
     return 0;
 }
